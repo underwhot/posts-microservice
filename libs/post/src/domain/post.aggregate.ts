dc4f8b2a61447a21b9ff685c7518b1347863a897
Artem Petrovskiy <artvpart@gmail.com>
2024-08-11 22:05:26 +0300

    ADD: domain validator

--- libs/post/src/domain/post.aggregate.ts
+++ libs/post/src/domain/post.aggregate.ts
@@ -1,14 +1,38 @@
-import { AggregateRoot } from '@nestjs/cqrs';
 import { IPost } from './post.interface';
 import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
+import { PostServices } from './services';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsString,
+  IsUUID,
+  validateSync,
+} from 'class-validator';
+import { Exclude } from 'class-transformer';
 
-export class PostAggregate extends AggregateRoot implements IPost {
+export class PostAggregate extends PostServices implements IPost {
+  @IsUUID()
   id: string = randomStringGenerator();
+
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
   message: string;
+
+  @IsUUID()
   authorId: string;
-  isPublished: boolean = false;
+
+  @IsBoolean()
+  @Exclude()
+  isPublished = false;
+
+  @IsString()
   updatedAt = new Date().toISOString();
+
+  @IsString()
   createdAt = new Date().toISOString();
 
   private constructor() {
@@ -18,6 +42,11 @@ export class PostAggregate extends AggregateRoot implements IPost {
   static create(post: Partial<IPost>) {
     const _post = new PostAggregate();
     Object.assign(_post, post);
+    _post.updatedAt = post?.id ? new Date().toISOString() : _post.updatedAt;
+    const errors = validateSync(_post, { whitelist: true });
+    if (!!errors.length) {
+      throw new Error('Post validation failed');
+    }
     return _post;
   }
 }
---
+++ libs/post/src/domain/services/index.ts
@@ -0,0 +1,11 @@
+import { AggregateRoot } from '@nestjs/cqrs';
+import { ISetNotPublished, SET_NOT_PUBLISHED } from './set-not-published.case';
+import { ISetPublished, SET_PUBLISHED } from './set-published.case';
+
+export class PostServices
+  extends AggregateRoot
+  implements ISetNotPublished, ISetPublished
+{
+  setNotPublished = SET_NOT_PUBLISHED;
+  setPublished = SET_PUBLISHED;
+}
