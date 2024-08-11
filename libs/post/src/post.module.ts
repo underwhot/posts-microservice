0b29e5ca3b3bda945ea3bebeff4f920654509e9f
Artem Petrovskiy <artvpart@gmail.com>
2024-08-11 22:38:17 +0300

    UPD: init cqrs

--- libs/post/src/post.module.ts
+++ libs/post/src/post.module.ts
@@ -1,7 +1,30 @@
-import { Module } from '@nestjs/common';
+import { PostEntity } from '@lib/entities';
+import { Module, OnModuleInit } from '@nestjs/common';
+import { CommandBus, CqrsModule, EventBus, QueryBus } from '@nestjs/cqrs';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { POST_COMMANDS_HANDLERS } from './application-services/commands';
+import { POST_EVENTS_HANDLERS } from './application-services/events';
+import { POST_QUERIES_HANDLERS } from './application-services/queries';
+import { PostFacade } from './application-services';
 
 @Module({
-  providers: [],
-  exports: [],
+  imports: [CqrsModule, TypeOrmModule.forFeature([PostEntity])],
+  providers: [
+    ...POST_COMMANDS_HANDLERS,
+    ...POST_QUERIES_HANDLERS,
+    ...POST_EVENTS_HANDLERS,
+  ],
+  exports: [PostFacade],
 })
-export class PostModule {}
+export class PostModule implements OnModuleInit {
+  constructor(
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
+    private readonly eventBus: EventBus,
+  ) {}
+  onModuleInit() {
+    this.commandBus.register(POST_COMMANDS_HANDLERS);
+    this.queryBus.register(POST_QUERIES_HANDLERS);
+    this.eventBus.register(POST_EVENTS_HANDLERS);
+  }
+}
