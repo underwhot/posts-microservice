fce0af73d3bd4e9179503aa17163da8831417359
Artem Petrovskiy <artvpart@gmail.com>
2024-08-11 22:22:59 +0300

    ADD: all exeption filter

--- libs/shared/src/shared.module.ts
+++ libs/shared/src/shared.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { AllExcentionsFilter } from './filters';
 
 @Module({
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExcentionsFilter,
+    },
+  ],
   exports: [],
 })
 export class SharedModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { ProvidersModule } from '@lib/providers';
+import { SharedModule } from '@lib/shared';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [ProvidersModule],
+  imports: [SharedModule, ProvidersModule],
   controllers: [],
   providers: [],
 })
