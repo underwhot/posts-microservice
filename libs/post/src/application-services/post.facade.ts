ac3052cc288d0ef3ebc59d6f8e768a5e9d0fe166
Artem Petrovskiy <artvpart@gmail.com>
2024-08-12 22:29:20 +0300

    UPD: facade. Add commands and queries

--- libs/post/src/application-services/post.facade.ts
+++ libs/post/src/application-services/post.facade.ts
@@ -1,5 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CommandBus, EventBus, QueryBus } from '@nestjs/cqrs';
+import { CreatePostDto, UpdatePostDto } from './commands/dto';
+import {
+  CreatePostCommand,
+  CreatePostCommandHandler,
+  DeletePosCommandtHandler,
+  DeletePostCommand,
+  SetPublishedCommand,
+  SetPublishedCommandHandler,
+  UpdatePostCommand,
+  UpdatePostCommandHandler,
+} from './commands';
+import { GetPostQuery } from './queries/get-post/get-post.query';
+import { PaginationDto } from '@lib/shared/dto';
+import {
+  GetPostQueryHandler,
+  GetPostsQuery,
+  GetPostsQueryHandler,
+} from './queries';
 
 @Injectable()
 export class PostFacade {
@@ -9,7 +27,56 @@ export class PostFacade {
     private readonly eventBus: EventBus,
   ) {}
 
-  commands = {};
-  queries = {};
+  commands = {
+    createPost: (post: CreatePostDto) => this.createPost(post),
+    updatePost: (post: UpdatePostDto) => this.updatePost(post),
+    deletePost: (id: string) => this.deletePost(id),
+    setPublished: (id: string) => this.setPublishedPost(id),
+  };
+  queries = {
+    getOnePost: (id: string) => this.getPost(id),
+    getAllPosts: (pagination: PaginationDto) => this.getPosts(pagination),
+  };
   events = {};
+
+  private createPost(post: CreatePostDto) {
+    return this.commandBus.execute<
+      CreatePostCommand,
+      CreatePostCommandHandler['execute']
+    >(new CreatePostCommand(post));
+  }
+
+  private updatePost(post: UpdatePostDto) {
+    return this.commandBus.execute<
+      UpdatePostCommand,
+      UpdatePostCommandHandler['execute']
+    >(new UpdatePostCommand(post));
+  }
+
+  private setPublishedPost(id: string) {
+    return this.commandBus.execute<
+      SetPublishedCommand,
+      SetPublishedCommandHandler['execute']
+    >(new SetPublishedCommand(id));
+  }
+
+  private deletePost(id: string) {
+    return this.queryBus.execute<
+      DeletePostCommand,
+      DeletePosCommandtHandler['execute']
+    >(new DeletePostCommand(id));
+  }
+
+  private getPost(id: string) {
+    return this.queryBus.execute<GetPostQuery, GetPostQueryHandler['execute']>(
+      new GetPostQuery(id),
+    );
+  }
+
+  private getPosts(pagination: PaginationDto) {
+    return this.queryBus.execute<
+      GetPostsQuery,
+      GetPostsQueryHandler['execute']
+    >(new GetPostsQuery(pagination));
+  }
 }
