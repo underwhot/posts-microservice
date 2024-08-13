9aecb5ee31351421ca297ba63791444cdfca736c
Artem Petrovskiy <artvpart@gmail.com>
2024-08-13 22:31:48 +0300

    ADD: public decorator | jwt guard | update shared module

--- libs/auth/src/auth.module.ts
+++ libs/auth/src/auth.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtFactory } from './config';
+import { GUARDS } from './guards';
 
 @Module({
-  providers: [AuthService],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync(jwtFactory),
+  ],
+  providers: [AuthService, ...GUARDS],
   exports: [AuthService],
 })
 export class AuthModule {}
